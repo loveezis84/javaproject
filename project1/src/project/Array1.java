d5ebd5605c726031f9e82e391aeb42f8307de474
loveezis84 <loveezis84@gmail.com>
2023-04-14 11:15:37 +0900

    master

--- project1/src/project/Array1.java
+++ project1/src/project/Array1.java
@@ -1,10 +1,21 @@
 package project;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array1 {
 
+	//ArrayList 배열 문자,숫자 혼합 사용하기
 	public static void main(String[] args) {
-		System.out.println("test");
+		String data[] = {"hong","park"}; //문자배열
+		int data2[] = {10,20}; //숫자배열
+		//현재 배열 자료형이 2가지 입니다.
+		//해당 방식은 Object를 사용합니다
+		ArrayList<Object> li = new ArrayList<>(Arrays.asList(data));
+		System.out.println(li);
+		li.add(data2[0]);
+		li.add(data2[1]);
+		System.out.println(li);
 
 	}
-
 }
