8cd9353b290081d39b89ccc40b6b17dc20a57dfd
loveezis84 <loveezis84@gmail.com>
2023-04-14 11:51:15 +0900

    master

--- project1/src/project/Object2.java
+++ project1/src/project/Object2.java
@@ -1,6 +1,7 @@
 package project;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Object2 {
 
@@ -12,8 +13,24 @@ public class Object2 {
 		al.add(6000);
 		al.add("01023451235");
 		System.out.println(al);
-		dd
-
+		
+		
+		//2차 원시 배열 Object (문자,숫자)
+		Object data[][] = {
+				{"hong","kim","park","jang"},
+				{25,30,44,21}
+		};
+		//Object 자료형 선언시 원시 배열도 만들수 있음
+		System.out.println(Arrays.toString(data[1]));
+		
+		//복합적 Object 배열
+		
+		Object userinfo[] = {"hong",25,"서울","01023458777"};
+		
+		
+		
+		
+		
 	}
 
 }
